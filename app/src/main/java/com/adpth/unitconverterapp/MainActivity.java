aed9ff4fcb86cd37fd852a0114c24e5c2a1ed030
Vedangi <vedangidhole10@gmail.com>
2022-07-30 21:13:38 +0530

    Unit-Converter-App

---
+++ app/src/main/java/com/adpth/unitconverterapp/MainActivity.java
@@ -0,0 +1,35 @@
+package com.adpth.unitconverterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textview2;
+    private EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textview2 = findViewById(R.id.textView2);
+        editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = editText.getText().toString();
+                double m = Double.parseDouble(s);
+                double cm = 100 * m;
+                textview2.setText("The Centimetre Value is " + cm);
+                Toast.makeText(MainActivity.this, "The value is " + cm, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
